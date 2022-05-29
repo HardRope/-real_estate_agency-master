7b047355e7ea10e392639d8bd9b307d67e25db9e
HardRope <61661973+HardRope@users.noreply.github.com>
2022-05-29 05:04:53 +0300

    Fill with data field Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Appeal
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address',)
     readonly_fields = ['created_at',]
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone',)
     list_editable = ('new_building',)
     list_filter = ('new_building', 'town',)
     raw_id_fields = ('liked_by',)

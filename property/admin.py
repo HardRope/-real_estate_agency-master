7b354b8058728d0755a1543f8c44eda2ce7f72f0
HardRope <61661973+HardRope@users.noreply.github.com>
2022-06-07 01:57:11 +0300

    Delete duplicates

--- property/admin.py
+++ property/admin.py
@@ -11,9 +11,9 @@ class OwnerInline(admin.StackedInline):
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('flat_owner', 'town', 'address',)
+    search_fields = ('town', 'address', 'flat_owners__name')
     readonly_fields = ['created_at',]
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'town',)
     raw_id_fields = ('liked_by', 'flat_owners',)
--- property/models.py
+++ property/models.py
@@ -6,8 +6,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    flat_owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Номер владельца', blank=True)
 
     created_at = models.DateTimeField(
         'Когда создано объявление',
@@ -17,20 +15,24 @@ class Flat(models.Model):
     new_building = models.NullBooleanField('Новостройка', default=None, db_index=True)
 
     description = models.TextField('Текст объявления', blank=True)
+
     price = models.IntegerField('Цена квартиры', db_index=True)
 
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
         db_index=True)
+
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
         help_text='Чертаново Южное')
+
     address = models.TextField(
         'Адрес квартиры',
         help_text='ул. Подольских курсантов д.5 кв.4')
+
     floor = models.CharField(
         'Этаж',
         max_length=3,
@@ -39,6 +41,7 @@ class Flat(models.Model):
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
         db_index=True)
+
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
@@ -46,7 +49,9 @@ class Flat(models.Model):
         db_index=True)
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
+
     active = models.BooleanField('Активно-ли объявление', db_index=True)
+
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
@@ -57,8 +62,7 @@ class Flat(models.Model):
         User,
         verbose_name='Кто лайкнул',
         blank=True,
-        related_name="user_likes"
-    )
+        related_name="user_likes")
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
@@ -69,27 +73,27 @@ class Appeal(models.Model):
         User,
         on_delete=models.CASCADE,
         verbose_name='Кто жаловался',
-        related_name="user_appeals"
-    )
+        related_name="user_appeals")
+
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,
         verbose_name='Квартира, на которую пожаловались',
-        related_name="flat_appeals"
-    )
+        related_name="flat_appeals")
+
     text = models.TextField('Текст жалобы')
 
 
 class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owner_phonenumber = models.CharField('Номер владельца', max_length=20)
+
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+
     flats = models.ManyToManyField(
         Flat,
         verbose_name='Квартиры в собственности',
-        blank=True,
         related_name="flat_owners"
     )
 
     def __str__(self):
-        return self.name
+        return f'{self.name}, {self.owner_pure_phone}'
